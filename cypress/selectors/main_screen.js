f4561f2e330ff0b3d96684b9ad6d17408865b4d9
stanislav <polyakovst@gmail.com>
2022-07-26 18:21:14 +0300

    This is success with cypress

---
+++ cypress/selectors/main_screen.js
@@ -0,0 +1,5 @@
+export const main_screen = {
+    //Locators for elements on the main screen
+
+    logout_button: '[data-test="sidenav-signout"]'
+}
---
+++ cypress/selectors/main_screen_onboarding_popups.js
@@ -0,0 +1,15 @@
+export const main_screen_onboarding_popups = {
+    //Locators for elements in the start popups during the first login
+
+    //'Get Started with Real World App' popup:
+    next_button: '[data-test="user-onboarding-next"]',
+
+    // 'Create Bank Account' popup:
+    bank_name_field: '[data-test="bankaccount-bankName-input"]',
+    routing_number_field: '[data-test="bankaccount-routingNumber-input"]',
+    account_number_field: '[data-test="bankaccount-accountNumber-input"]',
+    bank_acc_save_button: '[data-test="bankaccount-submit"]',
+
+    //'Finished' popup
+    done_button: '[data-test="user-onboarding-next"]'
+}
