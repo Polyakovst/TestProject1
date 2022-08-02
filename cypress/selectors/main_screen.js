950eaf5329a4724f02947a9a0fd3c5c30d11fa34
stanislav <polyakovst@gmail.com>
2022-08-02 20:39:50 +0300

    This is success with cypress

--- cypress/selectors/main_screen.js
+++ cypress/selectors/main_screen.js
@@ -3,4 +3,6 @@ export const main_screen = {
 
     logout_button: '[data-test="sidenav-signout"]',
     bank_accounts_button: '[data-test="sidenav-bankaccounts"]',
+    new_transaction_button: '[href="/transaction/new"]',
+    mine_transactions_button: '[data-test="nav-personal-tab"]'
 }
---
+++ cypress/selectors/transactions_screen.js
@@ -0,0 +1,5 @@
+export const transactions_screen = {
+    //Locators for elements on transactions screen
+    transactions_list: '[class="ReactVirtualized__Grid ReactVirtualized__List"]'
+
+}
