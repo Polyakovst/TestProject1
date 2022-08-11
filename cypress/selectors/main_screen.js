1ae59e8b6f5d10f8f442043ac95d1e75c0bbd06f
Polyakovst <108526597+Polyakovst@users.noreply.github.com>
2022-08-11 15:12:47 +0300

    Revert "HomeWork5"

--- cypress/selectors/main_screen.js
+++ cypress/selectors/main_screen.js
@@ -2,7 +2,5 @@ export const main_screen = {
     logout_button: '[data-test="sidenav-signout"]',
     bank_accounts_button: '[data-test="sidenav-bankaccounts"]',
     new_transaction_button: '[href="/transaction/new"]',
-    mine_transactions_button: '[data-test="nav-personal-tab"]',
-    notifications_button: '[data-test="sidenav-notifications"]',
-    home_button: '[data-test="sidenav-home"]'
+    mine_transactions_button: '[data-test="nav-personal-tab"]'
 }
--- cypress/selectors/transactions_screen.js
+++ cypress/selectors/transactions_screen.js
@@ -1,9 +1,3 @@
 export const transactions_screen = {
-    transactions_list: '[class="ReactVirtualized__Grid ReactVirtualized__List"]',
-    first_transaction_in_the_list: '[data-test="transaction-item-183VHWyuQMS"]',
-    transaction_between_b_and_c: '[data-test="transaction-sender--7xanIywv9x"]',
-    like_button: '[data-test="transaction-like-button-183VHWyuQMS"]',
-    like_button_2: '[data-test="transaction-like-button--7xanIywv9x"]',
-    comment_field: '[id="transaction-comment-input-183VHWyuQMS"]',
-    comment_field_2: '[id="transaction-comment-input--7xanIywv9x"]'
+    transactions_list: '[class="ReactVirtualized__Grid ReactVirtualized__List"]'
 }
